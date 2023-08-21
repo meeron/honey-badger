fb9b047b5be131e444600df08cdacf3ad31bcc28
Miron Jakubowski <mijakubowski@gmail.com>
2023-08-21 19:07:30 +0200

    Get and set value

---
+++ db/db.go
@@ -0,0 +1,100 @@
+package db
+
+import (
+	"io"
+	"path"
+
+	"github.com/dgraph-io/badger/v4"
+)
+
+const DbBasePath = "./data"
+
+var dbs = make(map[string]*db_wrapp)
+
+type db_wrapp struct {
+	badger *badger.DB
+}
+
+type DbStats struct {
+	Lsm    int64
+	Vlog   int64
+	Tables []TableInfo
+}
+
+type TableInfo struct {
+	Id            uint64
+	KeyCount      uint32
+	OnDiskSize    uint32
+	StaleDataSize uint32
+}
+
+func (db *db_wrapp) Stats() DbStats {
+	lsm, vlog := db.badger.Size()
+
+	tables := make([]TableInfo, 0)
+	for _, t := range db.badger.Tables() {
+		tables = append(tables, TableInfo{
+			Id:            t.ID,
+			KeyCount:      t.KeyCount,
+			OnDiskSize:    t.OnDiskSize,
+			StaleDataSize: t.StaleDataSize,
+		})
+	}
+
+	return DbStats{
+		Lsm:    lsm,
+		Vlog:   vlog,
+		Tables: tables,
+	}
+}
+
+func (db *db_wrapp) Get(key string) ([]byte, error) {
+	var result []byte
+
+	err := db.badger.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		result, err = item.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
+func (db *db_wrapp) Set(key string, reader io.ReadCloser) error {
+	return db.badger.Update(func(txn *badger.Txn) error {
+		defer reader.Close()
+
+		data, err := io.ReadAll(reader)
+		if err != nil {
+			return err
+		}
+
+		return txn.Set([]byte(key), data)
+	})
+}
+
+func Get(name string) (*db_wrapp, error) {
+	db := dbs[name]
+	if db == nil {
+		dbPath := path.Join(DbBasePath, name)
+
+		bdb, err := badger.Open(badger.DefaultOptions(dbPath))
+		if err != nil {
+			return nil, err
+		}
+
+		dbs[name] = &db_wrapp{
+			badger: bdb,
+		}
+	}
+
+	return dbs[name], nil
+}
--- main.go
+++ main.go
@@ -1,24 +1,87 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
 
 	"github.com/dgraph-io/badger/v4"
 	"github.com/gorilla/mux"
+	"github.com/meeron/honey-badger/db"
 )
 
 func main() {
-	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from honey badger"))
-	})
+	}).Methods(http.MethodGet)
+	router.HandleFunc("/dbs/{name}", func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+
+		dbs, err := db.Get(params["name"])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		encoder := json.NewEncoder(w)
+		encoder.Encode(dbs.Stats())
+	}).Methods(http.MethodGet)
+	router.HandleFunc("/dbs/{name}/get", func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		query := r.URL.Query()
+
+		dbs, err := db.Get(params["name"])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		value, err := dbs.Get(query.Get("key"))
+		if err == badger.ErrKeyNotFound {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write(value)
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/dbs/{name}/set", func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		query := r.URL.Query()
+
+		key := query.Get("key")
+		if key == "" {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte(badger.ErrEmptyKey.Error()))
+			return
+		}
+
+		dbs, err := db.Get(params["name"])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		err = dbs.Set(key, r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write([]byte("Ok"))
+
+	}).Methods(http.MethodPost)
 
 	http.ListenAndServe(":8080", router)
 }
